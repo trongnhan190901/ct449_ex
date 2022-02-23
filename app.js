996e4a10c300e172042d83419d5e1e6802373bce
Trọng Nhân <58482915+trongnhan190901@users.noreply.github.com>
2022-02-24 00:01:14 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const setupContactRoutes = require('./app/routes/contact.routes')
+const { BadRequestError, errorHandler } = require("./app/errors")
 const { get } = require("express/lib/response");
 
 const app = express();
@@ -16,5 +17,15 @@ app.get("/", (req, res) => {
 })
 
 setupContactRoutes(app)
+
+
+app.use((req, res, next) => {
+    next(new BadRequestError(404, "Resource not found"))
+})
+
+app.use((err, req, res, next) => {
+    errorHandler.handleError(err, res)
+})
+
 
 module.exports = app;
